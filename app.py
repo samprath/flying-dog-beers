75f5b3cb60ff002d8490d671fff50f7f3ce117e5
samprath <53302999+samprath@users.noreply.github.com>
2019-07-26 12:38:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,52 +1,39 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.graph_objs as go
-
-########### Set up the chart
-beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
-ibu_values=[35, 60, 85, 75]
-abv_values=[5.4, 7.1, 9.2, 4.3]
-color1='lightblue'
-color2='darkgreen'
-
-bitterness = go.Bar(
-    x=beers,
-    y=ibu_values,
-    name='IBU',
-    marker={'color':color1}
-)
-alcohol = go.Bar(
-    x=beers,
-    y=abv_values,
-    name='ABV',
-    marker={'color':color2}
-)
-
-beer_data = [bitterness, alcohol]
-beer_layout = go.Layout(
-    barmode='group',
-    title = 'Beer Comparison'
-)
-
-beer_fig = go.Figure(data=beer_data, layout=beer_layout)
-
-########### Display the chart
-
 app = dash.Dash()
-server = app.server
-
-app.layout = html.Div(children=[
-    html.H1('Flying Dog Beers'),
-    dcc.Graph(
-        id='flyingdog',
-        figure=beer_fig
+colors = {
+    'background': '#111111',
+    'text': '#7FDBFF'
+}
+app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
+    html.H1(
+        children='Hello Dash',
+        style={
+            'textAlign': 'center',
+            'color': colors['text']
+        }
     ),
-    html.A('Code on Github', href='https://github.com/austinlasseter/flying-dog-beers'),
-    html.Br(),
-    html.A('Data Source', href='https://www.flyingdog.com/beers/'),
-    ]
-)
-
+    html.Div(children='Dash: A web application framework for Python 2.', style={
+        'textAlign': 'center',
+        'color': colors['text']
+    }),
+    dcc.Graph(
+        id='Graph1',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'MMM'},
+            ],
+            'layout': {
+                'plot_bgcolor': colors['background'],
+                'paper_bgcolor': colors['background'],
+                'font': {
+                    'color': colors['text']
+                }
+            }
+        }
+    )
+])
 if __name__ == '__main__':
     app.run_server()
